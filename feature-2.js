d9d1b4bcfa7e4d53af4c604d3fdf5eaa81a5cc96
bkrawczyk89 <krawczyk.blazej@gmail.com>
2021-09-13 12:42:32 +0200

    Added feature 2

---
+++ feature-2.js
@@ -0,0 +1,7 @@
+let feature2 = () => {
+	console.log("Feature 2")
+}
+
+
+export {feature2}
+
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+import feature2 from 'feature2.js'
+
 console.log("This is init function of the app")
+
+
+if(feature2){
+	feature2()
+}
