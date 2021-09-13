c7c1fb42b3ad23a9a7568a318d2bc4cb1e74f291
bkrawczyk89 <krawczyk.blazej@gmail.com>
2021-09-13 16:24:16 +0200

    Update nr 3

--- feature-2.js
+++ feature-2.js
@@ -2,6 +2,7 @@ let feature2 = () => {
 	console.log("Feature 2")
 	console.log("Update of feature 2...")
 	console.log("Second Update of feature 2")
+	console.log("Third Update of feature 2")
 }
 
 
